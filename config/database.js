a86762c452adc298ca50562c48d63bd2e88b58e1
shekhar <shekhar.suman@cybernetyx.com>
2018-12-03 20:26:23 +0530

    removed config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 module.exports = {
-    // configure the code below with your username, password and mlab database information
-    // change config before pushing to git
-    database: 'mongodb://suman:suman@ds255329.mlab.com:55329/meanauthapp', //prod
-    //database: 'mongodb://localhost:27017/meanauth',    //dev
-    secret: 'yoursecret'
-}
+  // configure the code below with your username, password and mlab database information
+  // change config before pushing to git
+  database: "mongodb://localhost:27017/meanauth", //dev
+  secret: "yoursecret"
+};
